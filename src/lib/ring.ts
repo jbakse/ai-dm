ee06583e714c111739d59769823a5d9940b6c8a1
Justin Bakse <justin@tsfim.com>
2023-10-17 15:25:36 -0500

    update openai api

--- src/lib/ring.ts
+++ src/lib/ring.ts
@@ -1,3 +1,4 @@
+import { sample, choose } from "./random";
 import { Item, makeItem } from "./item";
 import { postData } from "./network";
 import { buildPrompt } from "./util";
@@ -19,7 +20,12 @@ export function generateRing(): Ring {
 
 function generateRingData(): object {
   return {
-    properties: "gold",
+    properties: [
+      ...sample(["gold", "silver", "copper", "iron", "steel", "wood"], 1),
+      // ...sample(["shiny", "dull", "smooth", "rough", "heavy", "light"], 1),
+      ...sample(["simple", "ornate", "plain", "engraved"], 1),
+      ...sample(["magical", "mundane"], 1),
+    ],
   };
 }
 
@@ -27,7 +33,7 @@ export async function describeRing(
   ring: Ring,
   updateItem: (r: Ring) => void
 ): Promise<void> {
-  ring.name = await postData("/api/describeChat", {
+  const json_response = await postData("/api/describeChat", {
     functions: [
       {
         name: "populate_ring",
@@ -35,21 +41,38 @@ export async function describeRing(
         parameters: {
           type: "object",
           properties: {
-            name: { type: "string", description: "Name of the ring." },
+            name: {
+              type: "string",
+              description: "Name of the ring.",
+            },
             description: {
               type: "string",
               description: "Description of the ring.",
             },
-            required: ["name", "description"],
           },
+          required: ["name", "description"],
         },
       },
     ],
+    function_call: { name: "populate_ring" },
     prompt: buildPrompt`
     Populate the properties of a ring with the following properties:
 
     ${ring.data}
+
+    Make the name catchy. eg: 'Ring of the Dragon', 'Eric's Ring of Wishes'
+    Keep descriptions under 50 words.
+
+    If the ring is mundane, explicitly state that it is not magical.
+    If the ring is magical, alude to magic without specifics.
     `,
   });
+
+  const response = JSON.parse(json_response);
+  console.log(json_response, response);
+
+  ring.name = response.name;
+  ring.description = response.description;
+
   updateItem(ring);
 }
--- src/pages/api/describeChat.ts
+++ src/pages/api/describeChat.ts
@@ -1,26 +1,26 @@
-import { Configuration, OpenAIApi } from "openai";
+import OpenAI from "openai";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-const configuration = new Configuration({
+const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-const openai = new OpenAIApi(configuration);
 
 export default async function describe2(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log("describe2", req.body);
+  console.log("describe2", JSON.stringify(req.body));
 
   const model = "gpt-3.5-turbo";
-  const completion = await openai.createChatCompletion({
+  const response = await openai.chat.completions.create({
     model,
-    // functions: req.body.functions,
+    functions: req.body.functions || [],
+    function_call: req.body.function_call || "auto",
     messages: [
       {
         role: "system",
         content:
-          "You write descriptions of fictional items for a table top roll playing game. Your writing focuses on lore and physical descriptions and avoids mechanics and rules. You use real words. Respond with only what is asked for.",
+          "You write content for a table top roll playing game. Your writing focuses on lore and physical descriptions and avoids mechanics and rules. You use real words. The setting is high fantasy.",
       },
       {
         role: "user",
@@ -28,16 +28,21 @@ export default async function describe2(
       },
     ],
     temperature: 0.7, // 0 (focused) to 2 (creative)
-    max_tokens: 100,
+    max_tokens: 200,
   });
 
-  const prompt_tokens = completion.data.usage?.prompt_tokens ?? 0;
-  const completion_tokens = completion.data.usage?.completion_tokens ?? 0;
+  console.log("response", JSON.stringify(response, null, 2));
+
+  const prompt_tokens = response.usage?.prompt_tokens ?? 0;
+  const completion_tokens = response.usage?.completion_tokens ?? 0;
   const cost = ((prompt_tokens + completion_tokens) / 1000) * 0.002;
-  console.log(completion.data.choices[0].message);
+  // console.log(completion.data.choices[0].message);
 
   res.status(200).json({
-    result: completion.data.choices[0].message?.content.trim(),
+    result:
+      response.choices[0].message?.content?.trim() ||
+      response.choices[0].message?.function_call?.arguments,
+
     usage: {
       model,
       prompt_tokens,
